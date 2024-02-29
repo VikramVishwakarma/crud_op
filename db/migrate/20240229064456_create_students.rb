7366a646bf4a37e7048f3d74bf1abb19669f10e1
vikram <vikram2code@gmail.com>
2024-02-29 12:27:55 +0530

    first commit

---
+++ db/migrate/20240229064456_create_students.rb
@@ -0,0 +1,12 @@
+class CreateStudents < ActiveRecord::Migration[7.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :email
+      t.integer :phone
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
