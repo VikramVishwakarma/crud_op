1b60bc23542d89becd85c6c0a1dbca38721f0553
vikram <vikram2code@gmail.com>
2024-02-29 13:03:27 +0530

    Added email validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,16 @@
 class Student < ApplicationRecord
-end
+    validate :unique_email
+  
+    validates :phone, format: { with: /\A\d+\z/, message: "only allows numerical values" }
+    validates :name, presence: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" }, allow_blank: true
+  
+    private
+  
+    def unique_email
+      existing_student = Student.where.not(id: self.id).find_by(email: email)
+      if existing_student
+        errors.add(:email, "has already been taken")
+      end
+    end
+  end
+  
