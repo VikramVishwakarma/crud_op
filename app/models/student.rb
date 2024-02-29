1e31dae146d2561f8476b596eba6fd268ffd9623
vikram <vikram2code@gmail.com>
2024-02-29 14:31:22 +0530

    Added the validation on all fields and check the to database

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,9 @@
 class Student < ApplicationRecord
-    validate :unique_email
-  
-    validates :phone, format: { with: /\A\d+\z/, message: "only allows numerical values" }
+    validates :name, :address, :phone, presence: { message: "can't be blank" }
     validates :name, presence: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" }, allow_blank: true
+    validates :phone, presence: true, format: { with: /\A\d+\z/, message: "only allows numerical values" }, allow_blank: true
   
-    private
-  
-    def unique_email
-      existing_student = Student.where.not(id: self.id).find_by(email: email)
-      if existing_student
-        errors.add(:email, "has already been taken")
-      end
-    end
+    # Correcting email validation to include presence and uniqueness
+    validates :email, presence: true, uniqueness: { case_sensitive: false, message: "has already been taken" }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "is not a valid email address" }, allow_blank: true
   end
   
